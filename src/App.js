d21487bec9821cb219163a236fb762f0ff09c289
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-19 23:38:10 +0300

    Update Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import CoffeeList from './components/CoffeeList';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
