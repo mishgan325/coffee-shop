c4a03d830271307c1754c152a7a0eb833a054ee2
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-05-02 02:08:16 +0300

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Импортируем createRoot вместо render
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Создаем корень и рендерим приложение
 const root = ReactDOM.createRoot(document.getElementById('root'));
