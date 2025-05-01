c6f2d378781a0d5a1f2bf17f51ce4d1d3601128a
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-05-02 02:06:51 +0300

    Update styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Navbar = ({ cartCount }) => {
     return (
-        <nav>
-            <div>
-                <Link to="/">Главная</Link>
-            </div>
-            <div>
-                <Link to="/cart">Корзина ({cartCount})</Link>
-            </div>
-            <div>
-                <Link to="/orders">Мои заказы</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Кофе</Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarContent"
+                    aria-controls="navbarContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                Корзина ({cartCount})
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/orders">
+                                Мои заказы
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
 };
+
 
 export default Navbar;
